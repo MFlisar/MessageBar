827f6ffa32f589566268f096ec2cc826d34ffcc3
Michael <mflisar85@gmail.com>
2013-07-26 00:21:19 +0200

    demo adjusted

--- example/src/com/michaelflisar/messagebar/demo/MainActivity.java
+++ example/src/com/michaelflisar/messagebar/demo/MainActivity.java
@@ -1,6 +1,11 @@
 
 package com.michaelflisar.messagebar.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,7 +14,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.michaelflisar.messagebar.MessageBar;
@@ -25,55 +34,55 @@ public class MainActivity extends MessageBarActivity implements OnClickListener
 {
     // this message bar is not retained on screen rotation (see code of MessageBarActivity to see how this would be achieved)
     // short solution:
-    // just call mExtraMessageBar.onRestoreInstanceState(bundle) and 
+    // just call mExtraMessageBar.onRestoreInstanceState(bundle) and
     // mExtraMessageBar.onSaveInstanceState(bundle) in the corresponding functions of this activity
     private MessageBar mExtraMessageBar = null;
-   
+
     private OnMessageClickListener mListener = null;
-    
+
     private CheckBox checkBox;
-    
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        checkBox = (CheckBox)findViewById(R.id.cb);
-        
+
+        checkBox = (CheckBox) findViewById(R.id.cb);
+
         // from here on you can use the message bar
         // the custom message classes are jsut for convenience...
         // using the BaseMessage class is always possible
-        
+
         // HINTS:
         // for not showing buttons, set the button parameter to null
         // for not using images, set resId of image to -1
-        
+
         // if constructor for your message does not exist, you have following two possibilities:
         // 1) add the constructor and make a pull request
         // 2) create the message using the BaseMessage...
-        
+
         mListener = new OnMessageClickListener()
-        {   
+        {
             @Override
             public void onButton2Click(Parcelable data)
             {
-                Toast.makeText(MainActivity.this, "button 1 pressed", 3000).show();
+                Toast.makeText(MainActivity.this, "button 2 pressed", 3000).show();
             }
-            
+
             @Override
             public void onButton1Click(Parcelable data)
             {
-                Toast.makeText(MainActivity.this, "button 2 pressed", 3000).show();
+                Toast.makeText(MainActivity.this, "button 1 pressed", 3000).show();
             }
         };
     }
+
     private MessageBar getExtraMessageBar()
     {
         if (mExtraMessageBar == null)
             mExtraMessageBar = new MessageBar(findViewById(R.id.linearLayout1), true);
-        
+
         return mExtraMessageBar;
     }
 
@@ -84,10 +93,11 @@ public class MainActivity extends MessageBarActivity implements OnClickListener
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
         if (item.getItemId() == R.id.link)
         {
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://michaelflisar.github.io/MessageBar/"));
@@ -98,48 +108,66 @@ public class MainActivity extends MessageBarActivity implements OnClickListener
             getMessageBar().clear();
             getExtraMessageBar().clear();
         }
-        return true;   
+        return true;
     }
 
     @Override
     public void onClick(View v)
     {
-        BaseMessage message = null;
-        MessageBar mb = checkBox.isChecked() ? getExtraMessageBar() : getMessageBar();
+        final MessageBar mb = checkBox.isChecked() ? getExtraMessageBar() : getMessageBar();
+        final List<BaseMessage> messages = new ArrayList<BaseMessage>();
+
+        ArrayList<String> items = new ArrayList<String>();
+        items.add("add all messages");
+        items.add("add text message");
+        items.add("add text message with button");
+        items.add("add test message with image button");
+        items.add("add 2 button message with listener");
+        items.add("add constant message");
+        items.add("add timer message");
         
-        if (v.getId() == R.id.button1)
+        ListView lv = new ListView(this);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Make your selection");
+        builder.setView(lv);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
+        lv.setOnItemClickListener(new OnItemClickListener()
         {
-            message = new TextMessage("text only");
-        }
-        else if (v.getId() == R.id.button2)
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int pos, long arg3)
+            {
+                int i = 1;
+                if (pos == 0 || pos == i++)
+                    messages.add(new TextMessage("text only"));
+                if (pos == 0 || pos == i++)
+                    messages.add(new TextMessage("single button", "ok", null));
+                if (pos == 0 || pos == i++)
+                    messages.add(new TextMessage("single button", "ok", R.drawable.ic_launcher));
+                if (pos == 0 || pos == i++)
+                {
+                    TextMessage message = new TextMessage("double button (with listener)", "yes", "no");
+                    message.setClickListener(mListener);
+                    messages.add(message);
+                }
+                if (pos == 0 || pos == i++)
+                    messages.add(new ConstantMessage("constant message", "dismiss"));
+                if (pos == 0 || pos == i++)
+                    messages.add(new Timer2ButtonMessage("text with timer", "ok", null));
+            }
+        });
+        builder.setPositiveButton("back", new DialogInterface.OnClickListener()
         {
-            message = new TextMessage("single button", "ok", null);
-        }
-        else if (v.getId() == R.id.button3)
-        {
-            message = new TextMessage("single button", "ok", R.drawable.ic_launcher); 
-        }
-        else if (v.getId() == R.id.button4)
-        {
-            message = new TextMessage("double button (with listener)", "yes", "no");
-            message.setClickListener(mListener);
-        }
-        else if (v.getId() == R.id.button5)
-        {
-            message = new ConstantMessage("constant message", "dismiss");
-        }
-        else if (v.getId() == R.id.button6)
-        {
-            message = new Timer2ButtonMessage("text with timer", "ok", null);
-        }
-        
-        if (message != null)
-        {
-            message.show(mb);
-            // alternatively:
-            // mb.show(message);
-        }
-        
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                for (int i = 0; i < messages.size(); i++)
+                    messages.get(i).show(mb); // alternatively: mb.show(messages.get(i));
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
 }
